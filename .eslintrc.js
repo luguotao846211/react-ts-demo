081a5e140e7d6cb35c33d1d6a37d43f998af0252
luguotao <luguotao@meituan.com>
2020-08-18 15:39:00 +0800

    message

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    // env: {
+    //     "browser": true,
+    //     "es2020": true
+    // },
+    // extends: [
+    //     "plugin:react/recommended",
+    //     "airbnb",
+    //     "airbnb/hooks",
+    //     "plugin:@typescript-eslint/recommended"
+    // ],
+    // parser: "@typescript-eslint/parser",
+    // parserOptions: {
+    //     "ecmaFeatures": {
+    //         "jsx": true
+    //     },
+    //     "ecmaVersion": 11,
+    //     "sourceType": "module"
+    // },
+    // plugins: [
+    //     "react",
+    //     "@typescript-eslint"
+    // ],
+    // settings: {
+    //     'import/resolver': {
+    //         node: {
+    //             extensions: ['.tsx', '.ts', '.js', '.json'],
+    //         },
+    //     },
+    // },
+    // rules: {
+    //     'import/extensions': [
+    //         ERROR,
+    //         'ignorePackages',
+    //         {
+    //             ts: 'never',
+    //             tsx: 'never',
+    //             json: 'never',
+    //             js: 'never',
+    //         },
+    //     ],
+    // }
+};
---
+++ src/a.ts
@@ -0,0 +1,7 @@
+// var add = (a, b) => {
+//     console.log(a + b)
+//     return a + b
+//   }
+//   console.log("123")
+  
+//   export default add
